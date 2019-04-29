8fc7e13cb6db115bb2fef107383764f03ec59834
Michelle Moon <michellemoon236@gmail.com>
2019-04-29 22:20:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,8 +3,7 @@ class Triangle
   def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
-    @side3 = side3
-    
+
   end
   
   def kind 
@@ -19,8 +18,6 @@ class Triangle
     else 
       :isosceles
     end
-
-
   end
   
   class TriangleError < StandardError
